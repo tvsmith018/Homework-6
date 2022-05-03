d985ba1e51efbb1c53354d9572bf937c79be9cac
tvsmith018 <104206007+tvsmith018@users.noreply.github.com>
2022-05-03 12:45:54 -0400

    Add files via upload

---
+++ network_assignment_3/network_assignment_3/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  network_assignment_3
+//
+//  Created by Consultant on 5/1/22.
+//
+
+import Foundation
+import UIKit
+
+struct Pic_Info {
+    var author_name: String
+    var picture: UIImage
+    var picture_id: String
+}
+
---
+++ network_assignment_3/network_assignment_3/PictureAPI.swift
@@ -0,0 +1,20 @@
+//
+//  PictureAPI.swift
+//  network_assignment_3
+//
+//  Created by Consultant on 5/1/22.
+//
+
+import UIKit
+
+class PictureAPI {
+    
+    
+    
+    static func getPictures() -> [Pic_Info]{
+        let pic_data: [Pic_Info] = []
+        
+        return pic_data
+        
+    }
+}
